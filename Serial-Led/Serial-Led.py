d8beed730806d7cdc2c7fc3aa330174c648ff862
halina20011 <65724763+halina20011@users.noreply.github.com>
2021-05-11 20:24:07 +0200

    Update Serial-Led.py

--- Serial-Led/Serial-Led.py
+++ Serial-Led/Serial-Led.py
@@ -5,11 +5,11 @@
  #  YouTube: https://www.youtube.com/watch?v=QE2UWFv8szw&t
  #  
 #####################################################################################
-import serial
-import time
+import serial #Import Library
+import time   #Import Library
 
-arduino = serial.Serial('COM12', baudrate = 9600, timeout = 1) //Com may not be the same!!
-time.sleep(3)
+arduino = serial.Serial('COM12', baudrate = 9600, timeout = 1) #Com may not be the same!!
+time.sleep(3) #Sleep 3s
 
     
 while(1):
@@ -18,10 +18,10 @@ while(1):
     if (dataFromUser == "Off" or dataFromUser == "off" or dataFromUser == "f" or dataFromUser == "0"): 
         arduino.write(b'0') 
         print("LED turned OFF")
-        time.sleep(0.5) //Sleep 0.5s
+        time.sleep(0.5) #Sleep 0.5s
     
     if (dataFromUser == "On" or dataFromUser == "on" or dataFromUser == "n" or dataFromUser == "1"): 
         arduino.write(b'1') 
         print("LED turned ON")
-        time.sleep(0.5) //Sleep 0.5s
+        time.sleep(0.5) #Sleep 0.5s
 
