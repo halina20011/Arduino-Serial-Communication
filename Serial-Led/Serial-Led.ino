f4d4c89e8829ad4986f55d4edd87dbb91838dcf4
halina20011 <65724763+halina20011@users.noreply.github.com>
2021-05-11 20:22:38 +0200

    Add files via upload

---
+++ Serial-Led/Serial-Led.ino
@@ -0,0 +1,31 @@
+/*#################################################################################
+ * This project was produced by Halina
+ * 
+ *  github: https://github.com/halina20011/Arduino-Serial-Communication
+ *  YouTube: https://www.youtube.com/watch?v=QE2UWFv8szw&t
+ *  
+###################################################################################*/
+int ledPin = 13;
+int incomingByte = 2;
+String command = "";
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+void loop(){
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read(); //Read Serial port
+
+    Serial.print("Received: ");
+    Serial.println(incomingByte);
+    if (incomingByte == '1'){ //If the serial port sent 1, the LED will turn ON.
+      Serial.println("Led: ON");
+      digitalWrite(ledPin, HIGH); //Turn ON Led
+    }
+    if (incomingByte == '0'){ //If the serial port sent 1, the LED will turn OFF.
+      Serial.println("Led: OFF");
+      digitalWrite(ledPin, LOW); //Turn OFF Led
+    }
+  }
+}
---
+++ Serial-Led/Serial-Led.py
@@ -0,0 +1,27 @@
+###################################################################################
+ # This project was produced by Halina
+ # 
+ #  github: https://github.com/halina20011/Arduino-Serial-Communication
+ #  YouTube: https://www.youtube.com/watch?v=QE2UWFv8szw&t
+ #  
+#####################################################################################
+import serial
+import time
+
+arduino = serial.Serial('COM12', baudrate = 9600, timeout = 1) //Com may not be the same!!
+time.sleep(3)
+
+    
+while(1):
+    dataFromUser = input("-> ")
+
+    if (dataFromUser == "Off" or dataFromUser == "off" or dataFromUser == "f" or dataFromUser == "0"): 
+        arduino.write(b'0') 
+        print("LED turned OFF")
+        time.sleep(0.5) //Sleep 0.5s
+    
+    if (dataFromUser == "On" or dataFromUser == "on" or dataFromUser == "n" or dataFromUser == "1"): 
+        arduino.write(b'1') 
+        print("LED turned ON")
+        time.sleep(0.5) //Sleep 0.5s
+
